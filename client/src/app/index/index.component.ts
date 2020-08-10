b63ecf9fea89402eb4de535baea289ba3c8d4b02
matfija <lazar1997@mts.rs>
2020-08-10 22:03:50 +0200

    Vidljivost lozinki na klijentu

--- client/src/app/index/index.component.ts
+++ client/src/app/index/index.component.ts
@@ -224,6 +224,19 @@ export class IndexComponent implements OnInit, OnDestroy {
     this.modalDugme.nativeElement.click();
   }
 
+  // Promena vidljivosti lozinki i ikonice
+  public promeniVidljivost(polje: HTMLInputElement, ikona: HTMLElement): void {
+    if (polje.type === 'password') {
+      polje.type = 'text';
+      ikona.classList.remove('fa-lock');
+      ikona.classList.add('fa-unlock');
+    } else {
+      polje.type = 'password';
+      ikona.classList.remove('fa-unlock');
+      ikona.classList.add('fa-lock');
+    }
+ }
+
   // Dohvatanje polja formulara
   public get pAlas() {
     return this.prijavaFormular.get('alas');
