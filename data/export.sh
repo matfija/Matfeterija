3eb7eac90ec75e768c7b64cc571c23b9f6be9f4f
matfija <lazar1997@mts.rs>
2020-08-10 03:50:48 +0200

    Čuvanje dva korisnika pre uvođenja autentikacije preko mejla

--- data/export.sh
+++ data/export.sh
@@ -1,3 +1,3 @@
-mongoexport --db matfeterija --collection users --out users.json
-mongoexport --db matfeterija --collection posts --out posts.json
-mongoexport --db matfeterija --collection comms --out comms.json
+mongoexport --db matfeterija --collection users --out users.json --pretty
+mongoexport --db matfeterija --collection posts --out posts.json --pretty
+mongoexport --db matfeterija --collection comms --out comms.json --pretty
