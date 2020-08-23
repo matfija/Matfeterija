c33a6cf439093a92a7edbcc28660ca06d1d8884e
pantelic-dusan <pantelic.dusan@protonmail.com>
2020-08-23 18:51:05 +0200

    izgled komentara

--- client/src/app/components/comment-view/comment-view.component.ts
+++ client/src/app/components/comment-view/comment-view.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy, OnChanges, EventEmitter, Output } from '@angular/core';
+import { UserService } from 'src/app/data.services/user.service';
+import { Subscription } from 'rxjs';
+import { Comment } from './../../interfaces/comment.model';
+import { CommentService } from 'src/app/data.services/comment.service';
 
 @Component({
   selector: 'app-comment-view',
@@ -6,14 +10,67 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./comment-view.component.css']
 })
 
-export class CommentViewComponent implements OnInit {
+export class CommentViewComponent implements OnInit, OnDestroy, OnChanges {
+
+  private pretplate: Subscription[] = [];
 
   @Input()
   public komentar: Comment;
 
-  constructor() { }
+  @Output()
+  public osvezavanjeObjaveEvent = new EventEmitter();
+
+  public korisnikLajkovao: boolean;
+
+  constructor(public userService: UserService,
+              public commentService: CommentService) { }
 
   ngOnInit() {
+    if(this.komentar.likes.includes(this.userService.korisnikPodaci._id)) {
+      this.korisnikLajkovao = true;
+    } else {
+      this.korisnikLajkovao = false;
+    }
+  }
+
+  ngOnDestroy() {
+    // Otkazivanje svih pretplata kako ne bi curela memorija
+    this.pretplate.forEach(pretplata => pretplata.unsubscribe());
+  }
+
+  ngOnChanges() {
+    if(this.komentar.likes.includes(this.userService.korisnikPodaci._id)) {
+      this.korisnikLajkovao = true;
+    } else {
+      this.korisnikLajkovao = false;
+    }
+  }
+
+  lajkujKomentar() {
+    this.pretplate.push(
+      this.commentService.lajkujKomentar(this.komentar._id).subscribe((komentar) => {
+        if(this.korisnikLajkovao) {
+          this.korisnikLajkovao = false;
+        } else {
+          this.korisnikLajkovao = true;
+        }
+        this.osvezavanjeObjaveEvent.emit();
+        console.log(komentar);
+      }, (greska) => {
+        console.log(greska);
+      })
+    )
+  }
+
+  obrisiKomentar() {
+    this.pretplate.push(
+      this.commentService.obrisiKomentar(this.komentar._id).subscribe((komentar) => {
+        this.osvezavanjeObjaveEvent.emit();
+        console.log(this.komentar)
+      }, (greska) => {
+        console.log(greska);
+      })
+    )
   }
 
 }
